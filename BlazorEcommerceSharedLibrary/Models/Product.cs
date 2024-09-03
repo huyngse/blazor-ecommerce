67643606d9f9beafbd9ff909baf8731c0540c315
huyngse <ngogiahuy775309@gmail.com>
2024-09-03 20:53:21 +0700

    Add product UI

--- BlazorEcommerceSharedLibrary/Models/Product.cs
+++ BlazorEcommerceSharedLibrary/Models/Product.cs
@@ -14,8 +14,9 @@ namespace BlazorEcommerceSharedLibrary.Models
         public decimal Price { get; set; } = 0;
         [Required, DisplayName("Product Image")]
         public string? Base64Img { get; set; }
+        [Required, Range(0, 99999)]
         public int Quantity { get; set; }
-        public bool Featured { get; set; }
+        public bool Featured { get; set; } = false;
         public DateTime UploadedDate { get; set; } = DateTime.Now;
 
     }
